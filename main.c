49b304e7720dc719cb2e7463e0ddf430253f6628
brogniE <enrico.brognera@gmail.com>
2021-11-11 17:18:54 +0100

    add function lose

--- main.c
+++ main.c
@@ -46,51 +46,49 @@ struct Piano_Gioco{
 void stampa_blocco(struct Blocco b);
 void stampa_blocchi(struct  Blocco *b);
 void flip_blocco(struct Blocco *b, int rot);
-void flip_blocco_0(struct Blocco *b);
 void inizializza_matrice(struct Piano_Gioco *m);
 void stampa_matrice(struct Piano_Gioco m);
 void inizializza_blocchi(struct Blocco *blocchi);
 void stampa_colore(int colore);
 
 int inserisci_blocco(struct Piano_Gioco *m, struct Blocco b);
-void caduta_blocco(struct Piano_Gioco *m, struct Blocco b, int piano);
+int caduta_blocco(struct Piano_Gioco *m, struct Blocco b, int piano);
 int verifica_posizione_blocco(struct Piano_Gioco *m, struct Blocco b, int piano);
-void inserisci_bloccco_posizione(struct Piano_Gioco *m, struct Blocco b, int piano);
+int inserisci_bloccco_posizione(struct Piano_Gioco *m, struct Blocco b, int piano);
 
 int main() {
     struct Blocco blocchi[N_BLOCCHI];
 
     struct Piano_Gioco player1;
     inizializza_matrice(&player1);
-    inizializza_blocchi(&blocchi);
+    inizializza_blocchi(&blocchi[0]);
 
     stampa_blocchi(blocchi);
 
     /*richiesta blocco, posizione e rotazione*/
 
-    blocchi[0].pos_x=-1;
-    flip_blocco(&blocchi[0],2);
-
+    blocchi[0].pos_x=0;
+    flip_blocco(&blocchi[0],1);
     inserisci_blocco(&player1, blocchi[0]);
     stampa_matrice(player1);
     printf("\n\n");
 
     blocchi[1].pos_x=5;
     flip_blocco(&blocchi[1],0);
-
     inserisci_blocco(&player1, blocchi[1]);
-
     stampa_matrice(player1);
-
     printf("\n\n");
 
-    blocchi[2].pos_x=5;
+    blocchi[2].pos_x=1;
     flip_blocco(&blocchi[2],2);
-
     inserisci_blocco(&player1, blocchi[2]);
-
     stampa_matrice(player1);
+    printf("\n\n");
 
+    blocchi[3].pos_x=4;
+    flip_blocco(&blocchi[3],0);
+    inserisci_blocco(&player1, blocchi[3]);
+    stampa_matrice(player1);
     printf("\n\n");
 
     /*ripeti il tutto*/
@@ -232,17 +230,20 @@ void inizializza_blocchi(struct Blocco *blocchi){
     blocchi[6]=t_rov;
 }
 
-void inserisci_bloccco_posizione(struct Piano_Gioco *m, struct Blocco b, int piano){
+int inserisci_bloccco_posizione(struct Piano_Gioco *m, struct Blocco b, int piano){
     int i, j;
-    for(i=3;i>=0;i--) {
-        for (j = 0; j < 4; j++) {
+    int perso=0;
+    for(i=3;i>=0 && perso==0;i--) {
+        for (j = 0; j < 4 && perso==0; j++) {
             if(piano-(3-i)>=0 && b.pos_x+j<N_COLONNE &&  b.pos_x+j>=0) {
                 /*sostituisco i blocchi vuoti del piano di gioco con i nuovi valori del blocco*/
                 if (piano - i % 3 >= 0 && m->matrice[piano - (3 - i)][b.pos_x + j] == 0)
                     m->matrice[piano - (3 - i)][b.pos_x + j] = b.forma[i][j];
-            }
+            }else if(piano-(3-i)<0 && b.forma[i][j]!=0)
+              perso=1;
         }
     }
+    return perso;
 }
 
 int verifica_posizione_blocco(struct Piano_Gioco *m, struct Blocco b, int piano){
@@ -258,19 +259,22 @@ int verifica_posizione_blocco(struct Piano_Gioco *m, struct Blocco b, int piano)
     return corretto;
 }
 
-void caduta_blocco(struct Piano_Gioco *m, struct Blocco b, int piano){
+int caduta_blocco(struct Piano_Gioco *m, struct Blocco b, int piano){
     int corretto=verifica_posizione_blocco(m, b, piano);
     if(corretto==1){
-        caduta_blocco(m,b,piano+1);
+        return caduta_blocco(m,b,piano+1);
     }
     else{
-        inserisci_bloccco_posizione(m,b,piano-1);
+        return inserisci_bloccco_posizione(m,b,piano-1);
     }
 }
 
 int inserisci_blocco(struct Piano_Gioco *m, struct Blocco b){
-    int perso=0;
-    caduta_blocco(m,b,0);
+    int perso;
+    perso=caduta_blocco(m,b,0);
+    if(perso==1){
+        printf("hai perso coglione AHAHAHAHAHAH \n\n");
+    }
     /*controllo se ho perso o se score aumenta*/
     return perso;
 }
