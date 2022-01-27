30c46e15918ec7e89cfa1e572e388146f18398ab
Bettiol <36667512+Bettiol@users.noreply.github.com>
2022-01-27 16:13:38 +0100

    Added Documentation

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 /**
  * @file main.c
  * @authors Bettiol Luca, Brognera Enrico
- * @mainpage main X-TETRIS
- * @section intro_sec Introduzione
+ * @mainpage X-TETRIS
+ * @section Introduzione
  *
  * Progetto universitario di Introduzione alla Programmazione, che consiste nella rielaborazione in ANSI-C del famoso gioco arcade Tetris
  *
@@ -17,6 +17,11 @@
  *      - Single Player
  *      - Multi Player
  *      - Player vs CPU
+ *
+ *
+ *
+ *
+ * Creato da Bettiol Luca (890424) e Brognera Enrico (890406)
  *
  */
 int main() {
